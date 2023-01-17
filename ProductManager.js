047cc78f9d8b896f75be4a301b5e144da6bd30b4
catani-maximiliano <Catanimaximiliano@gmail.com>
2023-01-17 18:10:27 -0300

    segunda-entrega

--- ProductManager.js
+++ ProductManager.js
@@ -1,27 +1,24 @@
-const fs = require("fs")
+const fs = require("fs");
+const path = "./ProductManager.json";
 
-const path = "./ProductManager.json"
-
-if(!fs.existsSync(path)){
-  fs.writeFileSync(path,JSON.stringify([], null, '\t'))
+if (!fs.existsSync(path)) {
+  fs.writeFileSync(path, JSON.stringify([], null, "\t"));
 }
- 
+
 class ProductManager {
   #Products = [];
-   
 
   constructor() {
-    this.id = 0
+    this.id = 0;
+    this.path = "./ProductManager.json";
   }
- 
- 
-  addProduct(title, description, price, thumbnail, code, stock) {
-    const productosObjeto= JSON.parse(fs.readFileSync(path,"utf-8")) 
-    console.log(productosObjeto.lastIndexOf()===-1)
 
- 
-    if (productosObjeto.length > 2) {
-      this.id++; 
+  addProduct(title, description, price, thumbnail, code, stock) {
+    const productosObjeto = JSON.parse(fs.readFileSync(this.path, "utf-8"));
+  
+
+    if (productosObjeto) {
+      this.id++;
       const producto = {
         id: this.id,
         title,
@@ -34,74 +31,72 @@ class ProductManager {
       const searchProduct = productosObjeto.find(
         (product) => product.code === code
       );
-      console.log(searchProduct)
-     
+      console.log(searchProduct);
+
       if (
-          !searchProduct &&
-            title &&
-            description &&
-            price &&
-            thumbnail &&
-            code &&
-            stock
-          ) {
-            productosObjeto.push(producto); 
-            fs.writeFileSync(path,JSON.stringify(productosObjeto, null, '\t'))
-            console.log(`Evento 1 creado con id: ${this.id}`);
-          } else {
-            console.log("ERROR: complete todos datos de entrada");
-          }
-    } else{
+        !searchProduct &&
+        title &&
+        description &&
+        price &&
+        thumbnail &&
+        code &&
+        stock
+      ) {
+        productosObjeto.push(producto);
+        fs.writeFileSync(
+          this.path,
+          JSON.stringify(productosObjeto, null, "\t")
+        );
+        console.log(`Evento 1 creado con id: ${this.id}`);
+      } else {
+        console.log("ERROR: ya existe el producto con este codigo");
+      }
+    } else {
       this.id++;
       const producto = {
         id: this.id,
-        title,
+        title, 
         description,
         price,
         thumbnail,
         code,
         stock,
       };
-    
-    if (
-      title &&
-      description &&
-      price &&
-      thumbnail &&
-      code &&
-      stock
-    ) {
-      productosObjeto.push(producto);
-      fs.writeFileSync(path,JSON.stringify(productosObjeto))
- 
-      console.log(`Evento creado con id: ${this.id}`);
-    } else {
-      console.log("ERROR: complete todos datos de entrada");
-    }}
+
+      if (title && description && price && thumbnail && code && stock) {
+        
+        fs.writeFileSync(this.path, JSON.stringify(producto));
+
+        console.log(`Evento creado con id: ${this.id}`);
+      } else {
+        console.log("ERROR: complete todos datos de entrada");
+      }
+    }
   }
 
   getProducts() {
-    const prod= JSON.parse(fs.readFileSync(path,"utf-8")) 
-    console.log(prod)
-  } 
+    const prod = JSON.parse(fs.readFileSync(this.path, "utf-8"));
+    console.log(prod);
+  }
 
   getProductById(idProduct) {
-    const productosOb= JSON.parse(fs.readFileSync(path,"utf-8")) 
-   
+    const productosOb = JSON.parse(fs.readFileSync(this.path, "utf-8"));
 
     const producto = productosOb.find((evento) => evento.id === idProduct);
 
     if (!producto) {
-      return console.log('no hay producto');
+      return console.log("no hay producto");
     }
 
     return console.log(producto);
   }
 
   updateProduct(idProduct, title, description, price, thumbnail, code, stock) {
-    const productosObjeto= JSON.parse(fs.readFileSync(path,"utf-8")) 
+    const productosObjeto = JSON.parse(fs.readFileSync(this.path, "utf-8"));
 
-    const findProducto = productosObjeto.find((evento) => evento.id === idProduct);
+    const findProducto = productosObjeto.find(
+      (evento) => evento.id === idProduct
+    );
     const indexProducto = productosObjeto.indexOf(findProducto);
 
     const product = {
@@ -113,34 +108,41 @@ class ProductManager {
       code,
       stock,
     };
+    if (findProducto) {
+      if (title && description && price && thumbnail && code && stock) {
+        productosObjeto.splice(indexProducto, 1, product);
+        fs.writeFileSync(
+          this.path,
+          JSON.stringify(productosObjeto, null, "\t")
+        );
 
-    if (
-      title &&
-      description &&
-      price &&
-      thumbnail &&
-      code &&
-      stock
-    ) {
-      productosObjeto.splice(indexProducto, 1, product);
-      fs.writeFileSync(path,JSON.stringify(productosObjeto, null, '\t'))
-
-      console.log(`Evento modificado con id: ${this.id}`);
-      return console.log(this.#Products[indexProducto])
+        console.log(`Evento modificado con id: ${this.id}`);
+        return console.log(productosObjeto[indexProducto]);
+      } else {
+        console.log("ERROR: complete todos datos de entrada");
+      }
     } else {
-      console.log("ERROR: complete todos datos de entrada");
+      console.log(`el producto con id ${idProduct} no existe`);
     }
   }
 
-  deleteProduct(idProduct){
-    const findProducto = this.#Products.find((evento) => evento.id === idProduct);
-    const indexProducto = this.#Products.indexOf(findProducto);
-    this.#Products.splice(indexProducto, 1);
+  deleteProduct(idProduct) {
+    const productosObjeto = JSON.parse(fs.readFileSync(this.path, "utf-8"));
 
+    const findProducto = productosObjeto.find(
+      (evento) => evento.id === idProduct
+    );
+    const indexProducto = productosObjeto.indexOf(findProducto);
+    if (findProducto) {
+      productosObjeto.splice(indexProducto, 1);
+      fs.writeFileSync(this.path, JSON.stringify(productosObjeto, null, "\t"));
+    } else {
+      console.log(`el producto con el id ${idProduct} no existe `);
+    }
   }
 }
- 
-// ----TESTING---- 
+
+// ----TESTING----
 
 const admProductos = new ProductManager();
 
@@ -180,10 +182,9 @@ comestibles.map((el) => {
     el.code,
     el.stock
   );
-}); 
+});
 
-admProductos.getProductById(1)
-admProductos.updateProduct(2,"naranjas","las mas ricas y jugosas",100,"naranja.com",24,100);
-admProductos.deleteProduct(3)
-admProductos.getProducts()
-  
+//admProductos.getProductById(1)
+//admProductos.updateProduct(2,"naranjas","las mas ricas y jugosas",100,"naranja.com",24,100);
+//admProductos.deleteProduct(2)
+//admProductos.getProducts()
